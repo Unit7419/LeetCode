b42a0e938d2787b4ca909abe67ec6dbc2ddb0d2d
summerUniq <1152972727@qq.com>
2020-12-04 15:50:30 +0800

    format

--- ReconstructionOfBinaryTree/index.ts
+++ ReconstructionOfBinaryTree/index.ts
@@ -3,7 +3,7 @@
  * @data 2020.12.04
  */
 
- /**
+/**
  * Definition for a binary tree node.
  * function TreeNode(val) {
  *     this.val = val;
@@ -12,8 +12,8 @@
  */
 
 function TreeNode(val) {
-        this.val = val;
-        this.left = this.right = null;
+  this.val = val;
+  this.left = this.right = null;
 }
 
 /**
@@ -21,22 +21,19 @@ function TreeNode(val) {
  * @param {number[]} inorder
  * @return {TreeNode}
  */
-export const buildTreeXq = function(preorder, inorder) {
-    if(preorder.length===0){
-        return null
-    }
-    // 根节点值
-    let root = preorder[0]
-    // 创建根节点
-    let node = new TreeNode(root)
-      // 找到根节点在中序遍历中的下标
-      const i = inorder.indexOf(root)
-      // 递归创建左右节点, 查找节点和下标的关系
-      node.left = buildTreeXq(preorder.slice(1, i+1), inorder.slice(0,i))
-      node.right = buildTreeXq(preorder.slice(i+1), inorder.slice(i+1))
-  
-  return node
-  };
+export const buildTreeXq = function (preorder, inorder) {
+  if (preorder.length === 0) {
+    return null;
+  }
+  // 根节点值
+  let root = preorder[0];
+  // 创建根节点
+  let node = new TreeNode(root);
+  // 找到根节点在中序遍历中的下标
+  const i = inorder.indexOf(root);
+  // 递归创建左右节点, 查找节点和下标的关系
+  node.left = buildTreeXq(preorder.slice(1, i + 1), inorder.slice(0, i));
+  node.right = buildTreeXq(preorder.slice(i + 1), inorder.slice(i + 1));
 
-
- 
+  return node;
+};
