3a9a4959242999ba894cbfbbba9876397b4cbaf0
justwink <rollawaypoint@gmail.com>
2020-12-04 17:46:41 +0800

    chore(test): 支持jest测试指定用例 yarn test [path], path 为空即为全部用例

--- ReconstructionOfBinaryTree/solution.jw.ts
+++ ReconstructionOfBinaryTree/solution.jw.ts
@@ -1,3 +1,18 @@
+type TreeN = {
+  val: number;
+  left: TreeN;
+  right: TreeN;
+} | null;
+
+type buildTree = (
+  preorder: any,
+  inorder: any,
+  preBegin?: number,
+  preEnd?: number,
+  inBegin?: number,
+  inEnd?: number
+) => TreeN;
+
 export const buildTree: buildTree = (
   p,
   i,
