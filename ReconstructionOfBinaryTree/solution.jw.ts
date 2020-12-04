802d24f864c34b9086cda5b435e167b6ab159326
justwink <rollawaypoint@gmail.com>
2020-12-04 16:31:40 +0800

    Add solution

---
+++ ReconstructionOfBinaryTree/solution.jw.ts
@@ -0,0 +1,24 @@
+export const buildTree: buildTree = (
+  p,
+  i,
+  p1 = 0,
+  p2 = p.length - 1,
+  i1 = 0,
+  i2 = i.length - 1
+) => {
+  const r = (...ps) => buildTree(p, i, ...ps);
+
+  const val = p[p1];
+  const pos = i.indexOf(val);
+  const ll = pos - i1;
+
+  return p1 >= p2 || i1 >= i2
+    ? p1 === p2 && i1 === i2
+      ? { val, left: null, right: null }
+      : null
+    : {
+        val,
+        left: r(p1 + 1, p1 + ll, i1, pos - 1),
+        right: r(p1 + ll + 1, p2, pos + 1, i2),
+      };
+};
