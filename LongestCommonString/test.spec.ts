6aa7e2bbaebfc9f98e6dfbca72a0df482dbb5199
christian <rollawaypoint@gmail.com>
2020-12-07 00:04:38 +0800

    Add solution

--- LongestCommonString/test.spec.ts
+++ LongestCommonString/test.spec.ts
@@ -1,3 +1,8 @@
+import { longestCommonString as longestCommonStringJw } from './solution.jw';
+
 test('最长公共子序列', () => {
-  expect(1).toBe(1);
+  expect(longestCommonStringJw('1234567', '1357')).toBe(4);
+  expect(longestCommonStringJw('abc', 'abc')).toBe(3);
+  expect(longestCommonStringJw('123', '456')).toBe(0);
+  expect(longestCommonStringJw('asd', 'dfg')).toBe(1);
 });
