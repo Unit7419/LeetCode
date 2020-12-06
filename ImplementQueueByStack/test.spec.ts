73ec401e953d5ad86ed226ce972752e8299027e1
justwink <rollawaypoint@gmail.com>
2020-12-06 14:08:48 +0800

    Add solution

--- ImplementQueueByStack/test.spec.ts
+++ ImplementQueueByStack/test.spec.ts
@@ -1,3 +1,15 @@
-test('重建二叉树', () => {
-  expect(1).toBe(1);
+import { CQueue as CQueueJw } from './solution.jw';
+
+test('两个栈实现队列', () => {
+  const queue = new CQueueJw();
+
+  queue.appendTail(3);
+  queue.appendTail(4);
+  queue.deleteHead();
+  queue.deleteHead();
+  queue.deleteHead();
+
+  expect(JSON.stringify(queue.logs)).toBe(
+    JSON.stringify([null, null, null, 3, 4, -1])
+  );
 });
