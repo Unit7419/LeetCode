404c55a1c67e8a269f461bbaeedb9a813fe68231
justwink <rollawaypoint@gmail.com>
2020-12-06 10:45:49 +0800

    Optional union keys' answer

--- optional-keys/type.test.ts
+++ optional-keys/type.test.ts
@@ -1,4 +1,8 @@
 import { Equal } from '@type-challenges/utils';
 import { Expect } from '@type-challenges/utils';
 
-type cases = [];
+type cases = [
+  Expect<
+    Equal<import('./index').OptionalKeys<{ name; age?; gn? }>, 'age' | 'gn'>
+  >
+];
