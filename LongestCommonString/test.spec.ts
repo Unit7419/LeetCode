bce67a9126dd5577d6704f1d46e77cc3989aa475
elegantYU <845384559@qq.com>
2020-12-07 18:01:50 +0800

    test: pass

--- LongestCommonString/test.spec.ts
+++ LongestCommonString/test.spec.ts
@@ -1,5 +1,6 @@
 import { longestCommonString as longestCommonStringJw } from './solution.jw';
 import { longestCommonSubsequence} from './solution.xq';
+import { longestCommonSubsequence as longestCommonSubsequenceEy } from './index'
 
 test('最长公共子序列', () => {
   expect(longestCommonStringJw('1234567', '1357')).toBe(4);
@@ -15,3 +16,10 @@ test('最长公共子序列', () => {
   expect(longestCommonSubsequence('123', '456')).toBe(0);
   expect(longestCommonSubsequence('asd', 'dfg')).toBe(1);
 });
+
+test('最长公共子序列', () => {
+  expect(longestCommonSubsequenceEy('1234567', '1357')).toBe(4);
+  expect(longestCommonSubsequenceEy('abc', 'abc')).toBe(3);
+  expect(longestCommonSubsequenceEy('123', '456')).toBe(0);
+  expect(longestCommonSubsequenceEy('asd', 'dfg')).toBe(1);
+})
