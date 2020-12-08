4f9f7c1f857cc3b48adb350fbcf1f7ab40294d40
elegantYU <845384559@qq.com>
2020-12-08 21:11:06 +0800

    feat: 我好了

--- LongestContinuousCommonString/test.spec.ts
+++ LongestContinuousCommonString/test.spec.ts
@@ -1,5 +1,6 @@
 import { longestContinuousCommonString as longestContinuousCommonStringJw } from '.';
 import { longestContinuousCommonStringXq } from './solution.xq';
+import { LongestContinuousCommonStringEy } from './solution.ey';
 
 test('Longest continuous common string.', () => {
   expect(longestContinuousCommonStringJw('12345078', '345678')).toBe('345');
@@ -13,4 +14,11 @@ test('Longest continuous common string xq', () => {
   expect(longestContinuousCommonStringXq('0034007800', '345678')).toBe('34');
   expect(longestContinuousCommonStringXq('12345', '567890')).toBe('5');
   expect(longestContinuousCommonStringXq('12345', '67890')).toBe(-1);
+});
+
+test('Longest continuous common string1', () => {
+  expect(LongestContinuousCommonStringEy('12345078', '345678')).toBe('345');
+  expect(LongestContinuousCommonStringEy('0034007800', '345678')).toBe('34');
+  expect(LongestContinuousCommonStringEy('12345', '567890')).toBe('5');
+  expect(LongestContinuousCommonStringEy('12345', '67890')).toBe(-1);
 });
