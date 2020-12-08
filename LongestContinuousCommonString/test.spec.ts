95b336cfcd669e57a6746a2c735f4fcd6eb5a2cb
Jay Zangwill <359390851@qq.com>
2020-12-08 22:15:37 +0800

    feat: 添加最长连续公共字符串题解

--- LongestContinuousCommonString/test.spec.ts
+++ LongestContinuousCommonString/test.spec.ts
@@ -1,5 +1,6 @@
 import { longestContinuousCommonString as longestContinuousCommonStringJw } from '.';
 import { LongestContinuousCommonStringEy } from './solution.ey'
+import { longestContinuousCommonStringJZ } from './soultion.jz'
 
 test('Longest continuous common string.', () => {
   expect(longestContinuousCommonStringJw('12345078', '345678')).toBe('345');
@@ -15,3 +16,9 @@ test('Longest continuous common string1', () => {
   expect(LongestContinuousCommonStringEy('12345', '67890')).toBe(-1);
 });
 
+test('Longest continuous common string2', () => {
+  expect(LongestContinuousCommonStringEy('12345078', '345678')).toBe('345');
+  expect(LongestContinuousCommonStringEy('0034007800', '345678')).toBe('34');
+  expect(LongestContinuousCommonStringEy('12345', '567890')).toBe('5');
+  expect(LongestContinuousCommonStringEy('12345', '67890')).toBe(-1);
+});
