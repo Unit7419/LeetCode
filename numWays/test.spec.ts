3fe64e9d1b1deb60e2e28051542e68d892ccaa4f
christian <rollawaypoint@gmail.com>
2020-12-09 00:14:32 +0800

    Add solution

--- numWays/test.spec.ts
+++ numWays/test.spec.ts
@@ -1,1 +1,12 @@
 // 单元测试
+import { numWays as numWaysJw } from './solution.jw';
+
+test('f(n) = f(n-1) + f(n-2)', () => {
+  expect(numWaysJw(7)).toBe(21);
+  expect(numWaysJw(17)).toBe(2584);
+  expect(numWaysJw(27)).toBe(317811);
+  expect(numWaysJw(45)).toBe(836311896);
+  expect(numWaysJw(46)).toBe(971215059);
+  expect(numWaysJw(77)).toBe(261180706);
+  // expect(numWaysJw(78)).toBe(923369890); 923369889 Precision error at n >= 78 0-77 works fine
+});
