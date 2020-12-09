74f9791643655ff682630dba0957dc77cede82a4
Jay Zangwill <359390851@qq.com>
2020-12-09 22:38:33 +0800

    feat: 添加青蛙跳台阶题解

--- numWays/test.spec.ts
+++ numWays/test.spec.ts
@@ -1,6 +1,7 @@
 // 单元测试
 import {numWays as numWaysXq} from './solution.xq'
 import { numWaysEy } from './solution.ey'
+import { numWaysJZ } from './solution.jz'
 
 test('numWays', ()=> {
     expect(numWaysXq(1)).toBe(1)
@@ -26,4 +27,17 @@ test('numWays1', ()=> {
     expect(numWaysEy(7)).toBe(21)
     expect(numWaysEy(78)).toBe(923369890)
     expect(numWaysEy(100)).toBe(782204094)
+})
+
+test('numWays2', ()=> {
+    expect(numWaysJZ(1)).toBe(1)
+    expect(numWaysJZ(0)).toBe(1)
+    expect(numWaysJZ(2)).toBe(2)
+    expect(numWaysJZ(3)).toBe(3)
+    expect(numWaysJZ(4)).toBe(5)
+    expect(numWaysJZ(5)).toBe(8)
+    expect(numWaysJZ(6)).toBe(13)
+    expect(numWaysJZ(7)).toBe(21)
+    expect(numWaysJZ(78)).toBe(923369890)
+    expect(numWaysJZ(100)).toBe(782204094)
 })
