b8c79b6af3c2c4f7453d925c5f68c416280c8603
summerUniq <1152972727@qq.com>
2020-12-11 16:28:40 +0800

    补充幂是0的场景

--- pow/solution.xq.ts
+++ pow/solution.xq.ts
@@ -5,6 +5,9 @@
  */
  export function powXq(num1: number, num2: number) {
      let result = 1;
+     if(num2 === 0){
+         return 1
+     }
      for(var i = 1; i<= Math.abs(num2); i++){
          result *= num1
      }
--- pow/test.spec.ts
+++ pow/test.spec.ts
@@ -6,4 +6,6 @@ test('pow(10,10) tobe 10^10', () => {
     expect(powXq(10, 10)).toBe(10000000000)
     expect(powXq(10, -2)).toBe(0.01)
     expect(powXq(-2, 10)).toBe(1024)
+    expect(powXq(0, 0)).toBe(1)
+    expect(powXq(1, 0)).toBe(1)
 })
