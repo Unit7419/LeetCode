40634ac42b6feda865ba15999915568312e110ad
summerUniq <1152972727@qq.com>
2020-12-11 12:12:22 +0800

    pow 的实现

---
+++ pow/solution.xq.ts
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param num1 {}
+ * @param num2 
+ */
+ export function powXq(num1: number, num2: number) {
+     let result = 1;
+     for(var i = 1; i<= Math.abs(num2); i++){
+         result *= num1
+     }
+     return num2 > 0 ? result : 1/ result
+ }
