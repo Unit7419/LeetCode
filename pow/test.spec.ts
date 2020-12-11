04fc272c725b574e641a62eca0888cc55faa4e7e
Jay Zangwill <359390851@qq.com>
2020-12-11 23:50:21 +0800

    feat: 添加快速幂

--- pow/test.spec.ts
+++ pow/test.spec.ts
@@ -1,5 +1,6 @@
 // Unit test
 import { pow as powEy } from './solution.ey'
+import { powJZ } from './solution.jz'
 
 test('pow(10,10) tobe 10^10', () => {
   expect(powEy(10, 10)).toBe(10000000000)
@@ -7,4 +8,12 @@ test('pow(10,10) tobe 10^10', () => {
   expect(powEy(-2, 10)).toBe(1024)
   expect(powEy(0, 0)).toBe(1)
   expect(powEy(1, 0)).toBe(1)
-}) 
+})
+
+test('pow(10,10) tobe 10^10', () => {
+  expect(powJZ(10, 10)).toBe(10000000000)
+  expect(powJZ(10, -2)).toBe(0.01)
+  expect(powJZ(-2, 10)).toBe(1024)
+  expect(powJZ(0, 0)).toBe(1)
+  expect(powJZ(1, 0)).toBe(1)
+})
