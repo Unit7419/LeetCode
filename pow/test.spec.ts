bb62e9df8aedb9a3a2e2f9f4cb85328f9b9c4d4d
summerUniq <1152972727@qq.com>
2020-12-11 12:12:22 +0800

    pow 的实现

--- pow/test.spec.ts
+++ pow/test.spec.ts
@@ -1,1 +1,9 @@
 // Unit test
+
+import {powXq} from './solution.xq'
+
+test('pow(10,10) tobe 10^10', () => {
+    expect(powXq(10, 10)).toBe(10000000000)
+    expect(powXq(10, -2)).toBe(0.01)
+    expect(powXq(-2, 10)).toBe(1024)
+})
