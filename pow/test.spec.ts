bfe18b9b62a6990093efc6820404522ce64bdfa1
elegantYU <845384559@qq.com>
2020-12-11 19:35:14 +0800

    feat: 判断有点多

--- pow/test.spec.ts
+++ pow/test.spec.ts
@@ -1,1 +1,10 @@
 // Unit test
+import { pow as powEy } from './solution.ey'
+
+test('pow(10,10) tobe 10^10', () => {
+  expect(powEy(10, 10)).toBe(10000000000)
+  expect(powEy(10, -2)).toBe(0.01)
+  expect(powEy(-2, 10)).toBe(1024)
+  expect(powEy(0, 0)).toBe(1)
+  expect(powEy(1, 0)).toBe(1)
+}) 
