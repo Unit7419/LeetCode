abcf2f49743379fa442e2a901e7fa3a438708f01
elegantYU <845384559@qq.com>
2020-12-12 22:42:26 +0800

    feat: 使用状态判断 而非记录数据

--- pathExist/test.spec.ts
+++ pathExist/test.spec.ts
@@ -2,15 +2,49 @@
  * 单元测试
  */
 
- import {exist} from "./index"
- import { pathExistEy } from './solution.ey'
+import { exist } from './index';
+import { pathExistEy2 } from './solution.ey';
 
- test('if path exist', ()=> {
-     expect(exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED")).toBe(true)
-     expect(exist([["a","b"],["c","d"]], "abcd")).toBe(false)
- })
- 
- test('if path exist1', ()=> {
-    expect(pathExistEy([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED")).toBe(true)
-    expect(pathExistEy([["a","b"],["c","d"]], "abcd")).toBe(false)
-})
+test('if path exist', () => {
+  expect(
+    exist(
+      [
+        ['A', 'B', 'C', 'E'],
+        ['S', 'F', 'C', 'S'],
+        ['A', 'D', 'E', 'E'],
+      ],
+      'ABCCED'
+    )
+  ).toBe(true);
+  expect(
+    exist(
+      [
+        ['a', 'b'],
+        ['c', 'd'],
+      ],
+      'abcd'
+    )
+  ).toBe(false);
+});
+
+test('if path exist1', () => {
+  expect(
+    pathExistEy2(
+      [
+        ['A', 'B', 'C', 'E'],
+        ['S', 'F', 'C', 'S'],
+        ['A', 'D', 'E', 'E'],
+      ],
+      'ABCCED'
+    )
+  ).toBe(true);
+  expect(
+    pathExistEy2(
+      [
+        ['a', 'b'],
+        ['c', 'd'],
+      ],
+      'abcd'
+    )
+  ).toBe(false);
+});
