1328e330bb2f5b81ba4fdcc74200f0330aa391de
Jay Zangwill <359390851@qq.com>
2020-12-12 23:57:51 +0800

    feat: 添加矩阵中的路径

--- pathExist/test.spec.ts
+++ pathExist/test.spec.ts
@@ -4,13 +4,19 @@
 
  import {exist} from "./index"
  import { pathExistEy } from './solution.ey'
+ import { pathExistJz } from './soultion.jz'
 
  test('if path exist', ()=> {
      expect(exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED")).toBe(true)
      expect(exist([["a","b"],["c","d"]], "abcd")).toBe(false)
  })
- 
+
  test('if path exist1', ()=> {
     expect(pathExistEy([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED")).toBe(true)
     expect(pathExistEy([["a","b"],["c","d"]], "abcd")).toBe(false)
+})
+
+test('if path exist2', ()=> {
+    expect(pathExistJz([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED")).toBe(true)
+    expect(pathExistJz([["a","b"],["c","d"]], "abcd")).toBe(false)
 })
