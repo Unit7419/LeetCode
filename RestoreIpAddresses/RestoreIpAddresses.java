dabadddf7f9580a7407b08ef17bfb5d0e9b1e118
Dr.Monster <591456101@qq.com>
2020-12-14 23:51:37 +0800

    遍历,暴力破解

--- RestoreIpAddresses/RestoreIpAddresses.java
+++ RestoreIpAddresses/RestoreIpAddresses.java
@@ -9,7 +9,7 @@ public class RestoreIpAddresses {
     @Test
     public void test(){
         String ips = "25525511135";
-//        String ips = "2552";
+//        String ips = "010010";
         List<String> stringList = restoreIpAddresses(ips);
         for(String item : stringList){
             System.out.println(item + "\n");
@@ -18,7 +18,7 @@ public class RestoreIpAddresses {
 
     public List<String> restoreIpAddresses(String s) {
         List<String> stringList = new ArrayList<>();
-        if(s.length() < 4){
+        if(s.length() < 4 || s.length() > 12){
             return stringList;
         }
 
@@ -37,17 +37,25 @@ public class RestoreIpAddresses {
         char[] checkChar = new char[2 * chars.length - 1];
 
         for(int i = 0 ; i < chars.length ; i++){
-            if(i % 2 == 1){
-                checkChar[i * 2] = ' ';
-            }else{
-                checkChar[i] = chars[i];
+            if(i != chars.length - 1){
+                checkChar[(i * 2) + 1] = '*';
             }
+            checkChar[i * 2] = chars[i];
         }
 
-        for(int i = 0 ; i < checkChar.length - 5 ; i++){
-            for(int j = 3 ; j < checkChar.length - 3 ; j++){
-                for (int k = 5 ; k < checkChar.length - 1 ; k++){
+//        for(int i = 1 ; i < checkChar.length - 5 ; i+=2){
+//            for(int j = 3 ; j < checkChar.length - 3 ; j+=2){
+//                for (int k = 5 ; k < checkChar.length - 1 ; k+=2){
+//                    char[] temp = checkChar;
+//                    buildIpAddress(i , j , k ,temp , stringList);
+//                }
+//            }
+//        }
 
+        for(int i = 0 ; i < chars.length - 3 ; i++){
+            for(int j = i + 1 ; j < chars.length - 2 ; j++){
+                for (int k = j + 1 ; k < chars.length - 1 ; k++){
+                    buildIpAddress(i , j , k ,chars , stringList);
                 }
             }
         }
@@ -56,9 +64,40 @@ public class RestoreIpAddresses {
     }
 
 
-    public List<String> buildIpAddress(char[] chars , int index , String subString){
+    public void buildIpAddress(int i , int j ,int k , char[] checkChar , List<String> stringList){
+        String str = "";
+        for(char item : checkChar){
+            if(item != '*'){
+                str += item;
+            }
+        }
 
-        return null;
+        String st1 = str.substring(0 , i + 1);
+        String st2 = str.substring(i + 1 , j + 1);
+        String st3 = str.substring(j + 1 , k + 1);
+        String st4 = str.substring(k + 1 , str.length());
+
+        if(checkNumber(st1) && checkNumber(st2) && checkNumber(st3) & checkNumber(st4)){
+            stringList.add(st1 + "." + st2 + "." + st3 + "." + st4);
+        }
     }
 
+
+    public boolean checkNumber(String s){
+        boolean result = false;
+
+        if(s.length() == 1 && (0 <= Integer.valueOf(s) && Integer.valueOf(s)<= 255)){
+            return true;
+        }
+
+        if(s.length() > 1  && s.length() < 4){
+            if((0 <= Integer.valueOf(s) && Integer.valueOf(s)<= 255) && !s.startsWith("0")){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        return result;
+    }
 }
