c95fec86577cafb593f15635d4a9c6f057ab08a1
justwink <rollawaypoint@gmail.com>
2020-12-15 17:05:33 +0800

    Update sln.ts

--- RestoreIpAddresses/sln.ts
+++ RestoreIpAddresses/sln.ts
@@ -1,3 +1,8 @@
+// s     原始字符串
+// data  收集符合条件的返回值
+// pos   每次递归截取的位置
+// chars 截取的字符（最终拼接为满足条件的IP
+// count 递归的次数（IP由四部分组成递归四次的时候就没必要再继续了
 export const sln = (s, data = [], pos = 0, chars = '', count = 0) => {
   const last = pos >= s.length || count === 4;
   const match =
