d6f6be357dd5e01c13520a2fb0ab1e59165256a7
summerUniq <1152972727@qq.com>
2020-12-16 10:32:36 +0800

    convert: 找字符串下标和数组下班的关系

--- Convert/test.spec.ts
+++ Convert/test.spec.ts
@@ -1,6 +1,14 @@
 import { convertEy } from './solution.ey';
+import {convertXq} from './solution.xq';
 
 test('convert Z', () => {
   expect(convertEy('LEETCODEISHIRING', 3)).toBe('LCIRETOESIIGEDHN');
   expect(convertEy('LEETCODEISHIRING', 4)).toBe('LDREOEIIECIHNTSG');
 });
+
+test('convert z', ()=> {
+  expect(convertXq('LEETCODEISHIRING', 3)).toBe('LCIRETOESIIGEDHN');
+  expect(convertXq('LEETCODEISHIRING', 4)).toBe('LDREOEIIECIHNTSG');
+  expect(convertXq('LEETCODEISHIRING', 1)).toBe('LEETCODEISHIRING');
+  expect(convertXq('L', 2)).toBe('L');
+})
