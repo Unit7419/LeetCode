d940401b087c324dc05865855a9a50bbdb61e30e
summerUniq <1152972727@qq.com>
2020-12-17 00:34:29 +0800

    写哭了

--- TransformIndent/test.spec.ts
+++ TransformIndent/test.spec.ts
@@ -1,16 +1,94 @@
 // Unit test
-import { convertTreeEy } from './solution.ey';
 
-const s1 = `1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import {transformIndent} from './solution.xq'
+var output = {
+    value: 'root',
+    children: [
+      {
+        value: '1',
+        children: [
+          {
+            value: '2',
+            children: [
+              {
+                value: '3',
+              }
+            ],
+          }
+        ],
+      },
+      {
+        value: '4',
+        children: [
+          {
+            value: '5',
+          },
+          {
+            value: '6',
+          }
+        ],
+      },
+      {
+        value: '7',
+      }
+    ],
+  }
+
+var input = `
+1
   2
     3
 4
   5
   6
-7`;
+7
+`
 
-test('文本缩进转树结构', () => {
-  expect(JSON.stringify(convertTreeEy(s1))).toBe(
-    `{"value":"root","children":[{"value":"1","children":[{"value":"2","children":[{"value":"3"}]}]},{"value":"4","children":[{"value":"5"},{"value":"6"}]},{"value":"7"}]}`
-  );
-});
+test('transform indent', ()=> {
+    expect(transformIndent(input)).toStrictEqual(output)
+})
