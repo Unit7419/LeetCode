34680759df2e7832a2a360f46a91e3c72a9c1eac
elegantYU <845384559@qq.com>
2020-12-16 22:20:28 +0800

    feat: 懂得都懂

--- TransformIndent/test.spec.ts
+++ TransformIndent/test.spec.ts
@@ -1,1 +1,16 @@
 // Unit test
+import { convertTreeEy } from './solution.ey';
+
+const s1 = `1
+  2
+    3
+4
+  5
+  6
+7`;
+
+test('文本缩进转树结构', () => {
+  expect(JSON.stringify(convertTreeEy(s1))).toBe(
+    `{"value":"root","children":[{"value":"1","children":[{"value":"2","children":[{"value":"3"}]}]},{"value":"4","children":[{"value":"5"},{"value":"6"}]},{"value":"7"}]}`
+  );
+});
