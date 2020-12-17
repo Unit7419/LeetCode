b85265b3d21b84e4d2843837088f69b6da27dad4
Jay Zangwill <359390851@qq.com>
2020-12-17 23:35:28 +0800

    feat: 最小路径和

--- minPathSum/test.spec.ts
+++ minPathSum/test.spec.ts
@@ -1,1 +1,6 @@
-// 
+import { minPathSumJZ } from './solution.jz'
+
+test('minPathSumJZ', ()=> {
+    expect(minPathSumJZ([[1,2,3],[4,5,6]])).toBe(12)
+    expect(minPathSumJZ([[1,3,1],[1,5,1],[4,2,1]])).toBe(7)
+})
