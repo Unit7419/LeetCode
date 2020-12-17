ce7ad6d97724e76c15afb0c0af4891f8c8825821
elegantYU <845384559@qq.com>
2020-12-17 20:39:06 +0800

    feat: 还是矩阵

--- minPathSum/test.spec.ts
+++ minPathSum/test.spec.ts
@@ -1,1 +1,12 @@
-// 
+//
+import { minPathSumEy } from './solution.ey';
+
+test('最短路径', () => {
+  expect(
+    minPathSumEy([
+      [1, 3, 1],
+      [1, 5, 1],
+      [4, 2, 1],
+    ])
+  ).toBe(7);
+});
