34e309f42a87929b0fc5d4b48fbcd4c876bd019e
summerUniq <1152972727@qq.com>
2020-12-17 19:55:04 +0800

    feat:  最小路径和

--- minPathSum/test.spec.ts
+++ minPathSum/test.spec.ts
@@ -1,1 +1,8 @@
 // 
+
+import {minPathSum as minPathSumXq} from './solution.xq'
+
+test('min path sum xq', ()=> {
+    expect(minPathSumXq([[1,3,1],[1,5,1],[4,2,1]])).toEqual(7)
+    expect(minPathSumXq([[1,2,3],[4,5,6]])).toEqual(12)
+})
