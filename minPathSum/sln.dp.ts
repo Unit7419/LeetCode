c86e0ee935a8c23313915ba658a5c52bf081eeea
christian <rollawaypoint@gmail.com>
2020-12-17 22:46:20 +0800

    Simplify

--- minPathSum/sln.dp.ts
+++ minPathSum/sln.dp.ts
@@ -1,16 +1,9 @@
-export const minPath = matrix => {
-  const dp = Array.from(Array(matrix.length), () =>
-    Array(matrix[0].length).fill(0)
-  );
-
-  dp[0][0] = matrix[0][0];
-
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (j && !i) dp[i][j] = dp[i][j - 1] + matrix[i][j];
-      if (i && !j) dp[i][j] = dp[i - 1][j] + matrix[i][j];
-      if (i && j)
-        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + matrix[i][j];
+export const minPath = dp => {
+  for (let i = 0; i < dp.length; i++) {
+    for (let j = 0; j < dp[i].length; j++) {
+      if (j && !i) dp[i][j] = dp[i][j - 1] + dp[i][j];
+      if (i && !j) dp[i][j] = dp[i - 1][j] + dp[i][j];
+      if (i && j) dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + dp[i][j];
     }
   }
 
