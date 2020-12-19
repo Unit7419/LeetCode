638c3e3865891860e15b75b9dbeb7cba803e6b0b
summerUniq <1152972727@qq.com>
2020-12-19 16:05:14 +0800

    最大子序列和

--- maxSum/test.spec.js
+++ maxSum/test.spec.js
@@ -1,5 +1,10 @@
 import {maxSubArray as maxSubArrayJw} from './sln.jw'
+import {maxSumXq} from './solution.xq'
 
 test('maxSubArray',() => {
   expect(maxSubArrayJw([-2,1,-3,4,-1,2,1,-5,4])).toBe(6)
+})
+
+test('max sub array xq', ()=> {
+  expect(maxSumXq([-2,1,-3,4,-1,2,1,-5,4])).toBe(6)
 })
