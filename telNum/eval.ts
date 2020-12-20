0e20b8711f3c253b291d860497c5b3e8aaf2a3bb
justwink <rollawaypoint@gmail.com>
2020-12-20 17:03:18 +0800

    style format

--- telNum/eval.ts
+++ telNum/eval.ts
@@ -1,37 +1,9 @@
 // yarn node telNum/eval.ts
 // npm run node telNum/eval.ts
 
-var global = {}
-var varsList = []
-var data = []
-
-function push() {
-  return varsList.reduce((_, [vars, idx]) => {
-    return `${_ ? _ + '+' : _}global.${vars}[${idx}]`
-  }, '')
-}
-
-
-function ex (...args) {
-  var [exp, end] = args
-
-  if (end) {
-    return exp.replace('$$', `data.push(${push()})`)
-  }
-
-  return exp
-}
-
-function g(list, varsIdx, ...args) {
-  return `
-    for (let ${varsIdx} = 0; ${varsIdx} < global.${list}.length; ${varsIdx}++) {
-      ${ex(...args)}
-      ${args[2] ? '$$' : ''}
-    }
-  
-  `
-}
-
+var global = {};
+var varsList = [];
+var data = [];
 var mappings = {
   2: 'abc',
   3: 'def',
@@ -43,32 +15,54 @@ var mappings = {
   9: 'wxyz',
 };
 
+function push() {
+  return varsList.reduce((_, [vars, idx]) => {
+    return `${_ ? _ + '+' : _}global.${vars}[${idx}]`;
+  }, '');
+}
+
+function ex(...args) {
+  var [exp, end] = args;
+
+  return end ? exp.replace('$$', `data.push(${push()})`) : exp;
+}
+
+function g(list, varsIdx, ...args) {
+  return `
+    for (let ${varsIdx} = 0; ${varsIdx} < global.${list}.length; ${varsIdx}++) {
+      ${ex(...args)}
+      ${args[2] ? '$$' : ''}
+    }
+  
+  `;
+}
+
 function get(input) {
-  return [...input].reduce((_, n) => [..._, [...mappings[n]]], [])
+  return [...input].reduce((_, n) => [..._, [...mappings[n]]], []);
 }
 
 function forEach(input, fors = '') {
-  var x = get(input)
+  var x = get(input);
 
   x.forEach((_, idx) => {
-    var vars = `list${idx}`
-    var varsIdx = `l${idx}`
-  
-    varsList.push([vars, varsIdx])
-    global[vars] = _
-    fors = g(vars, varsIdx, fors, idx === x.length - 1, idx === 0)
-  })
+    var vars = `list${idx}`;
+    var varsIdx = `l${idx}`;
 
-  return fors
+    varsList.push([vars, varsIdx]);
+    global[vars] = _;
+    fors = g(vars, varsIdx, fors, idx === x.length - 1, idx === 0);
+  });
+
+  return fors;
 }
 
-function run (input) {
-  eval(forEach(input))
+function run(input) {
+  eval(forEach(input));
 
-  return data
+  return data;
 }
 
-console.log(run('23'))
+console.log(run('23'));
 // output:
 // [
 //   'ad', 'bd', 'cd',
