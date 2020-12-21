7c2eba6f94a016a1638d6322f9e13ec75e185cae
justwink <rollawaypoint@gmail.com>
2020-12-21 21:05:20 +0800

    Sln of bestSeqAtIndex

--- CircusTowerLcci/sln.jw.heap.out.of.ts
+++ CircusTowerLcci/sln.jw.heap.out.of.ts
@@ -6,13 +6,11 @@ const map = (arr, k) =>
     .sort((a, b) => a[k] - b[k])
     .map(_ => _.id);
 
+
 export const bestSeqAtIndexJw = (
   h,
   w,
   merge = h.map((v, i) => ({ h: v, w: w[i], id: i }))
 ) => longestCommonString(map(merge, 'h'), map(merge, 'w'));
-
-bestSeqAtIndexJw([65, 70, 56, 75, 60, 68], [100, 150, 90, 190, 95, 110]);
-// => 6
 
 // length 1w 时 heap out of memory
