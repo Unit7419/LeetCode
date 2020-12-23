73bf246c1f4d4fbc6426365cbb297e6da8a446f4
Jay Zangwill <359390851@qq.com>
2020-12-23 22:51:32 +0800

    feat: 添加dom树的遍历题解

--- ThreeSumClosest/test.spec.ts
+++ ThreeSumClosest/test.spec.ts
@@ -1,5 +1,10 @@
 import { threeSumClosest as threeSumClosestJw } from './sln.jw';
+import { threeSumClosestJZ } from './solution.jz';
 
 test('threeSumClosest', () => {
   expect(threeSumClosestJw([-1, 2, 1, -4], 1)).toBe(2);
+});
+
+test('threeSumClosest1', () => {
+  expect(threeSumClosestJZ([-1, 2, 1, -4], 1)).toBe(2);
 });
