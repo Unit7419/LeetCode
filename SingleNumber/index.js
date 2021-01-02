6819b1e120d7814a8fae2e0af1727306cfd26219
elegantYU <845384559@qq.com>
2021-01-02 22:39:41 +0800

    补作业 数组唯一不重复值&四数之和

--- SingleNumber/index.js
+++ SingleNumber/index.js
@@ -2,6 +2,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function(nums) {
+var singleNumber = function (nums) {
+  const obj = {}
 
+  for (let i = 0; i < nums.length; i++) {
+    obj[nums[i]] = (obj[nums[i]] || 0) + 1;
+  }
+
+  const singleIdx = Object.values(obj).findIndex(v => v === 1);
+
+  return Object.keys(obj)[singleIdx];
 };
