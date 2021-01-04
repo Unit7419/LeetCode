3757e59d6b2e898a0a0c88b64d40977be9d7e8b9
summerUniq <1152972727@qq.com>
2021-01-04 21:30:20 +0800

    feat:singleNumber ^

--- SingleNumber/sl.xq.ts
+++ SingleNumber/sl.xq.ts
@@ -2,15 +2,28 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var singleNumber = function(nums) {
+//     if(!nums || nums.length === 0){
+//         return 
+//     }
+//     const res = nums.reduce((_, cur) => {
+//         _[cur] = _[cur]?  _[cur]+1 : 1
+//         return {..._}
+//     }, {})
+//    return Object.keys(res).find((v)=> {
+//         return res[v] === 1
+//     })
+// };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// 使用异或，相同或异或是0， 0和任何数异或是数本身
 var singleNumber = function(nums) {
-    if(!nums || nums.length === 0){
-        return 
-    }
-    const res = nums.reduce((_, cur) => {
-        _[cur] = _[cur]?  _[cur]+1 : 1
-        return {..._}
-    }, {})
-   return Object.keys(res).find((v)=> {
-        return res[v] === 1
+    let ans = 0
+    nums.map((num)=> {
+        ans ^= num
     })
+    return ans
 };
