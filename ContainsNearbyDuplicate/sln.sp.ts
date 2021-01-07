2b4128c5d4254dba50d3d5d3cb574fbbcec4139f
sunpeng <ex-sunpeng002@pingan.com.cn>
2021-01-08 00:08:09 +0800

    again

--- ContainsNearbyDuplicate/sln.sp.ts
+++ ContainsNearbyDuplicate/sln.sp.ts
@@ -1,4 +1,4 @@
-function containsNearDu(arr, key) {
+function containsNearDu(arr, k) {
 	return arr.some((item, i) => {
 		const sliceArr = arr.slice(i + 1, i + 1 + k);
 		return sliceArr.includes(item);
